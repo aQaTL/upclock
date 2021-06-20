47d2295824724569c7ea326cf9dde0cfc5aa59b5
aQaTL <mmsoltys@outlook.com>
2021-06-21 00:36:07 +0200

    Pretty-print the uptime

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use anyhow::Result;
+use humantime::format_duration;
+use std::convert::TryInto;
+use std::time::Duration;
 
 fn main() -> Result<()> {
 	let uptime = uptime()?;
-	println!("Up for {} seconds", uptime);
+	println!("Up for {}", format_duration(uptime));
 	Ok(())
 }
 
 #[cfg(target_os = "macos")]
-fn uptime() -> std::io::Result<i64> {
+fn uptime() -> Result<Duration> {
 	let mut uptime = libc::timespec {
 		tv_sec: 0,
 		tv_nsec: 0,
@@ -16,7 +19,7 @@ fn uptime() -> std::io::Result<i64> {
 	if unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut uptime as *mut libc::timespec) }
 		!= 0
 	{
-		return Err(std::io::Error::last_os_error());
+		return Err(std::io::Error::last_os_error().into());
 	}
-	return Ok(uptime.tv_sec);
+	return Ok(Duration::from_secs(uptime.tv_sec.try_into()?));
 }
